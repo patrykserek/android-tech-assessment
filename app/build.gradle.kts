2b2e90731080578883ca5c2a50f0c1b455f1dfe9
Patryk Serek <patryk.serek@tooploox.com>
2024-07-25 17:07:54 +0200

    build: Add `collectAsStateWithLifecycle()` method

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose-android:2.8.4")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
