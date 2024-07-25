c8af310436e354cba9ea6e9e632d21373ed6dcb8
Patryk Serek <patryk.serek@tooploox.com>
2024-07-25 16:10:01 +0200

    di: Add missing annotation

--- app/src/main/java/com/pelagohealth/codingchallenge/domain/dispatcher/DefaultDispatcherProvider.kt
+++ app/src/main/java/com/pelagohealth/codingchallenge/domain/dispatcher/DefaultDispatcherProvider.kt
@@ -2,8 +2,9 @@ package com.pelagohealth.codingchallenge.domain.dispatcher
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class DefaultDispatcherProvider : DispatcherProvider {
+class DefaultDispatcherProvider @Inject constructor() : DispatcherProvider {
     override val io: CoroutineDispatcher = Dispatchers.IO
     override val main: CoroutineDispatcher = Dispatchers.Main
     override val default: CoroutineDispatcher = Dispatchers.Default
