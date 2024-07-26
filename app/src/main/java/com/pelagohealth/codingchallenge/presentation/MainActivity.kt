3685e5e6f6ca9bf6833f4b7b19815adc4bef1d8e
Patryk Serek <patryk.serek@tooploox.com>
2024-07-26 16:53:44 +0200

    feat: Improve swipe experience

--- app/src/main/java/com/pelagohealth/codingchallenge/presentation/MainActivity.kt
+++ app/src/main/java/com/pelagohealth/codingchallenge/presentation/MainActivity.kt
@@ -3,8 +3,12 @@ package com.pelagohealth.codingchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,11 +16,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.SwipeToDismiss
@@ -26,6 +34,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -127,24 +136,61 @@ fun SwipeToDismissItem(
     SwipeToDismiss(
         modifier = modifier,
         state = swipeToDismissState,
-        background = {},
-        dismissContent = {
-            TextCard(
-                text = text,
-                backgroundColor = MaterialTheme.colorScheme.secondaryContainer,
+        background = {
+            val direction = swipeToDismissState.dismissDirection ?: return@SwipeToDismiss
+            val backgroundColor by animateColorAsState(
+                when (swipeToDismissState.targetValue) {
+                    DismissValue.Default ->
+                        MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f)
+
+                    else -> MaterialTheme.colorScheme.errorContainer
+                }
             )
+            val iconColor by animateColorAsState(
+                when (swipeToDismissState.targetValue) {
+                    DismissValue.Default ->
+                        MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f)
+
+                    else -> MaterialTheme.colorScheme.onErrorContainer
+                }
+            )
+            val alignment = when (direction) {
+                DismissDirection.StartToEnd -> Alignment.CenterStart
+                DismissDirection.EndToStart -> Alignment.CenterEnd
+            }
+            val scale by animateFloatAsState(
+                if (swipeToDismissState.targetValue == DismissValue.Default) 0.75f else 1f
+            )
+
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor, CardDefaults.shape),
+                contentAlignment = alignment,
+            ) {
+                Icon(
+                    Icons.Default.Delete,
+                    contentDescription = "Delete Icon",
+                    tint = iconColor,
+                    modifier = Modifier
+                        .scale(scale)
+                        .padding(horizontal = 16.dp),
+                )
+            }
         },
+        dismissContent = { TextCard(text = text) },
     )
 }
 
 @Composable
 fun TextCard(
     text: String,
-    backgroundColor: Color,
+    backgroundColor: Color = MaterialTheme.colorScheme.surfaceVariant,
 ) {
     Card(
         modifier = Modifier.thenIf(text.isBlank()) { shimmer() },
-        colors = CardDefaults.cardColors(containerColor = backgroundColor)
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+        elevation = CardDefaults.elevatedCardElevation(),
     ) {
         Text(
             modifier = Modifier
